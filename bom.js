5799eccc416f9aed6a3a3ed5b30217ae83a78bb3
akhila-vedicherla <vedicherlaakhila@gmail.com>
2025-05-14 20:25:54 +0530

    Add files via upload

---
+++ bom.js
@@ -0,0 +1,17 @@
+
+let userInput=prompt("enter a website name(instagram,facebook,or google")
+if(userInput=="instagram"){
+    window.open("https://www.instagram.com")
+
+}
+else if(userInput=="facebook")
+{
+    window.open("https://www.facebook.com")
+}
+else if(userInput=="google")
+{
+    window.open("https://www.google.com")
+}
+else {
+alert("Invalid input");
+}
